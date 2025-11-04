d38ca80894398aad4933b160bd53e95cb8427b5b
ansermgw <25158748+ansermgw@users.noreply.github.com>
2025-11-04 16:15:33 +0100

    feat: generate first real api file

--- scripts/generate_static_api.py
+++ scripts/generate_static_api.py
@@ -232,6 +232,8 @@ if __name__ == '__main__':
                         "time": lap['LapTime'],
                     }
 
+                    result['splits'].append(split)
+
                 race['results'].append(result)
 
             phase['races'].append(race)
